1bd26c52c741916acf7bc9b3e126501c1f8675b9
lyndontavares <integraldominio@gmail.com>
2018-07-27 11:28:49 -0300

    Ajustes diversos. Teste de protótipo para o ngx-builder.

--- src/test/java/com/idomine/freemaker/FreeMakerTeste02.java
+++ src/test/java/com/idomine/freemaker/FreeMakerTeste02.java
@@ -1,9 +1,13 @@
 package com.idomine.freemaker;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.idomine.NgBuilderApplication;
 import org.idomine.domain.crud.model.Projeto;
 import org.idomine.domain.crud.service.GenerationService;
 import org.idomine.domain.crud.service.helper.GeradorCrudHelper;
+import org.idomine.domain.crud.service.helper.TemplateBackendHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +20,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("tdd")
 public class FreeMakerTeste02
 {
-
     @Autowired
     private GenerationService generationService;
 
@@ -25,5 +28,20 @@ public class FreeMakerTeste02
     {
         GeradorCrudHelper.criarFolders();
         generationService.backendAllToOutput(Projeto.getFake());
+        copiarDataSql();
+    }
+
+    private void copiarDataSql()
+    {
+        String d =  "output/" + TemplateBackendHelper.BACKEND_APP_RESOURCE;
+        String o = "templates/" + TemplateBackendHelper.BACKEND_APP_RESOURCE;
+        try
+        {
+            GeradorCrudHelper.copyFile(new File(o + "data.sql"), new File(d + "data.sql"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
