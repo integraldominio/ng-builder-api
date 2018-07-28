f1a37606ffe375f5b91b828d579f9740912526fa
LYNDON-PC\lyndon <integraldominio@gmail.com>
2018-07-28 19:30:03 -0300

    Ajustes diversos.

--- src/main/java/org/idomine/domain/crud/model/Artefato.java
+++ src/main/java/org/idomine/domain/crud/model/Artefato.java
@@ -56,6 +56,8 @@ public class Artefato
     private String resourceName;
     private String className;
     private String classFolder;
+   
+    private boolean paginaHome;
 
     private String templateTs;
     private String templateCss;
