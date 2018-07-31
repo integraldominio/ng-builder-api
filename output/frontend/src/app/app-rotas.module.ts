de8d9853cb4c38e9bb273df927728db5e2329020
lyndontavares <integraldominio@gmail.com>
2018-07-31 11:09:58 -0300

    Ajustes diversos. Teste de protótipo para o ngx-builder.

--- output/frontend/src/app/app-rotas.module.ts
+++ output/frontend/src/app/app-rotas.module.ts
@@ -21,7 +21,7 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
  */
- 
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
--- output/frontend/src/app/shared/utilities/loose-invalid.ts
+++
@@ -1,3 +0,0 @@
-export function looseInvalid(a: string|number): boolean {
-  return a === '' || a === null || a === undefined;
-}
